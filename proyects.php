b186b1b4c6d73ca2cba799c9e0bb4248a339a415
Diego Cortes <cortesmendez.2003@gmail.com>
2024-03-04 16:30:33 -0600

    proyect 1.2

---
+++ proyects.php
@@ -0,0 +1,13 @@
+<?php require 'header.php'; ?>
+
+<section>
+  <div class="">
+    <div class="">
+      <div class="">
+        <h1>Proyectos terminados</h1>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php require 'footer.php'; ?>
