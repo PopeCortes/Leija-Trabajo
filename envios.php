6075a46577b66de76a71a1ad64ee7a765cd3a185
Diego Cortes <cortesmendez.2003@gmail.com>
2024-03-04 16:29:39 -0600

    vista de envios

---
+++ envios.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ЯLsolutions</title>
+    <!-- Link style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Link de Tailwind -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Link de Font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+    <header class="flex justify-between py-3 px-7 items-center">
+        <div class="">
+            <a href="index.php" class="font-semibold text-[2.3rem]">ЯLsolutions</a>
+        </div>
+        <div class="">
+            <ul class="flex space-x-8">
+                <li><a href="tel:+528781339622">+52 878 133 9622</a></li>
+                <li><a href="cart.php"><i class="fa-brands fa-shopify"></i></a></li>
+                <li><a href="">emrocl</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <section>
+        <div class="">
+            <div class="">
+                <div class="">
+                    <div class="">
+                        <form action="#" method="post">
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
